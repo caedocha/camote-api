51bc5434b4b50da8832d288e5c796aa3b41caa69
Carlos Chacon <caedo.00@gmail.com>
2017-02-19 21:19:28 -0600

    Update seed to create hummus recipe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+#
+# UNITS
+#
+
 units = [
   'teaspoon',
   'tablespoon',
@@ -11,7 +15,59 @@ units = [
   'ounce',
   'milligram',
   'gram',
-  'kilogram'
+  'kilogram',
+  'clove'
 ]
 
 units.each { |name| Unit.create(name: name) }
+
+#
+# INGREDIENTS
+#
+
+ingredients = [
+  'chickpeas',
+  'garlic',
+  'olive oil',
+  'lemon juice',
+  'tahini',
+  'salt',
+  'paprika'
+]
+
+ingredients.each { |name| Ingredient.create(name: name) }
+
+#
+# RECIPES
+#
+
+title = '5-minute Hummus'
+
+instructions = <<-EOF
+1) In a food processor, puree the chickpeas and garlic with the olive oil, lemon juice, tahini (if using), cumin, and ¾ teaspoon salt until smooth and creamy. Add 1 to 2 tablespoons water as necessary to achieve the desired consistency.
+2) Transfer to a bowl. Drizzle with olive oil and sprinkle with the paprika before serving.
+EOF
+
+recipe = Recipe.create(title: title, instructions: instructions, quantities: Quantity.all)
+
+#
+# QUANTITIES
+#
+
+quantities = [
+  [ 15, 'ounce', 'chickpeas' ],
+  [ 1, 'clove', 'garlic' ],
+  [ 0.25, 'cup', 'olive oil' ],
+  [ 2, 'tablespoon', 'lemon juice' ],
+  [ 2, 'tablespoon', 'tahini' ],
+  [ 1, 'teaspoon', 'cumin' ],
+  [ 1, 'teaspoon', 'salt' ],
+  [ 0.25, 'teaspoon', 'paprika' ]
+]
+
+quantities.each do |(amount, unit_name, ingredient_name)|
+  quantity = Quantity.create!(amount: amount, recipe: recipe)
+  quantity.units << Unit.where(name: unit_name)
+  quantity.ingredients << Ingredient.where(name: ingredient_name)
+  quantity.save
+end
